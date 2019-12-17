8f55c1ea8252ffc03f421c21824d159b91a4897a
emagdiev5 <emagdiev5@gmail.com>
2019-12-17 21:15:31 +0500

    removals

--- views/site/index.php
+++ views/site/index.php
@@ -1,28 +1,9 @@
 <?php
 
 /* @var $this yii\web\View */
-use kartik\grid\GridView;
-
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-	<div class="col-md-2">
-		<?php
-			echo \yii\widgets\ListView::widget()
 
-
-		?>
-	</div>
-	<div class="col-md-10">
-	<?php // Generate a bootstrap responsive striped table with row highlighted on hover
-	echo GridView::widget([
-	'dataProvider'=> $dataProvider,
-	'filterModel' => $searchModel,
-	'columns' => $gridColumns,
-	'responsive'=>true,
-	'hover'=>true
-	]);
-	?>
-	</div>
 </div>
